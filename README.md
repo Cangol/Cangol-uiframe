1a77f8019add7dcdc1bcbc9d107338682a62dc0a
Cangol <wxw404@gmail.com>
2016-04-14 21:36:23 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Cangol-app-uiframe
 Cangol-app-uiframe
+
+[![Download](https://api.bintray.com/packages/cangol/maven/Cangol-uiframe/images/download.svg) ](https://bintray.com/cangol/maven/Cangol-uiframe/_latestVersion)
+[![Build Status](https://travis-ci.org/Cangol/Cangol-uiframe.svg?branch=master)](https://travis-ci.org/Cangol/Cangol-uiframe)
--- uiframeDemo/src/mobi/cangol/mobile/demo/MainActivity.java
+++ uiframeDemo/src/mobi/cangol/mobile/demo/MainActivity.java
@@ -1,5 +1,6 @@
 package mobi.cangol.mobile.demo;
 
+import mobi.cangol.mobile.base.BaseActionBarActivity;
 import mobi.cangol.mobile.demo.fragment.HomeFragment;
 import mobi.cangol.mobile.demo.fragment.MenuFragment2;
 import mobi.cangol.mobile.logging.Log;
@@ -9,7 +10,7 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 @SuppressLint("ResourceAsColor")
-public class MainActivity extends SlidingNavigationFragmentActivity  {
+public class MainActivity extends BaseActionBarActivity  {
 	private static long back_pressed;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -18,20 +19,24 @@ public class MainActivity extends SlidingNavigationFragmentActivity  {
 //		this.setNavigationBarTintColor(R.color.black);
 		setContentView(R.layout.activity_main);
 		this.getCustomActionBar().setBackgroundResource(R.color.red);
-		if (savedInstanceState == null) {
-			this.setMenuFragment(MenuFragment2.class,null);
-			this.setContentFragment(HomeFragment.class, "HomeFragment", null);
-		}
+//		if (savedInstanceState == null) {
+//			this.setMenuFragment(MenuFragment2.class,null);
+//			this.setContentFragment(HomeFragment.class, "HomeFragment", null);
+//		}
 		findViews();
 		initViews(savedInstanceState);
 		initData(savedInstanceState);
-		this.setFloatActionBarEnabled(true);
+		//this.setFloatActionBarEnabled(true);
+        this.initFragmentStack(R.id.content_frame);
+        if(savedInstanceState==null)
+            this.replaceFragment(HomeFragment.class, "Home", null);
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
-		Log.v("onStart "+System.currentTimeMillis());
+		Log.v("onStart " + System.currentTimeMillis());
+
 	}
 
 	@Override
@@ -53,10 +58,10 @@ public class MainActivity extends SlidingNavigationFragmentActivity  {
 			app.exit();
 		}else{
 			back_pressed=System.currentTimeMillis();
+            showToast("Please on back");
 		}
 	}
 
-	@Override
 	public int getContentFrameId() {
 		return R.id.content_frame;
 	}
