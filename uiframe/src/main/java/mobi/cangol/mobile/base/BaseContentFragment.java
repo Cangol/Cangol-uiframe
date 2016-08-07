e7f2505e6e65571330201a6d92040091aacd8da7
Cangol <wxw404@gmail.com>
2016-08-07 19:55:42 +0800

    update license

--- uiframe/src/main/java/mobi/cangol/mobile/base/BaseContentFragment.java
+++ uiframe/src/main/java/mobi/cangol/mobile/base/BaseContentFragment.java
@@ -1,12 +1,12 @@
-/**
+/*
  * Copyright (c) 2013 Cangol
- * <p/>
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -70,9 +70,9 @@ public abstract class BaseContentFragment extends BaseFragment {
      *
      * @param title
      */
-    public void setTitle(String title) {
+    public void setTitle(int title) {
         if (this.getParentFragment() != null) return;
-        this.title = title;
+        this.title = getString(title);
         if (getCustomActionBar() != null)
             getCustomActionBar().setTitle(title);
     }
@@ -82,9 +82,9 @@ public abstract class BaseContentFragment extends BaseFragment {
      *
      * @param title
      */
-    public void setTitle(int title) {
+    public void setTitle(String title) {
         if (this.getParentFragment() != null) return;
-        this.title = getString(title);
+        this.title = title;
         if (getCustomActionBar() != null)
             getCustomActionBar().setTitle(title);
     }
