61829f639d60e86bba2ccc3b4834d91271b7b3a7
Cangol <wxw404@gmail.com>
2020-12-13 11:27:59 +0800

    review code

--- uiframe/src/main/java/mobi/cangol/mobile/navigation/DrawerMenuLayout.java
+++ uiframe/src/main/java/mobi/cangol/mobile/navigation/DrawerMenuLayout.java
@@ -21,10 +21,13 @@ import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Rect;
 import android.os.Build;
+
+import androidx.annotation.RequiresApi;
 import androidx.drawerlayout.widget.DrawerLayout;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.Surface;
@@ -241,11 +244,6 @@ public class DrawerMenuLayout extends DrawerLayout {
     }
 
     public void attachToActivity(Activity activity, boolean isFloatActionBarEnabled) {
-        // get the window background
-        TypedArray a = activity.getTheme().obtainStyledAttributes(new int[]{android.R.attr.windowBackground});
-        int background = a.getResourceId(0, 0);
-        a.recycle();
-
         this.isFloatActionBarEnabled = isFloatActionBarEnabled;
 
         if (isFloatActionBarEnabled) {
@@ -254,9 +252,6 @@ public class DrawerMenuLayout extends DrawerLayout {
             if (decorChild.getBackground() != null) {
                 this.setBackgroundDrawable(decorChild.getBackground());
                 decorChild.setBackgroundDrawable(null);
-            } else {
-                if (this.getBackground() == null)
-                    this.setBackgroundResource(background);
             }
             decor.removeView(decorChild);
             decor.addView(this, 0);
